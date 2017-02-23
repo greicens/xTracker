53bd067c23fed1dfc9936c8439bd8a268cf8c41e
greicens <gsilva@mail.ccsf.edu>
2017-02-22 23:55:21 -0800

    adds login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'welcome#index', as: "root"
+
+  resources :users
+  # SESSION ROUTES
+  get '/login' => 'sessions#new', as: 'login'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
