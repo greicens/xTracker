1e2b4fd674bf9d9f6ae82d3b1a15ccff50ae6dd9
greicens <gsilva@mail.ccsf.edu>
2017-02-24 13:56:33 -0800

    adds email and username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_uniqueness_of :username, :email
   include UsersHelper
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
