519ae2a433a5eaebd8700d0047cdfcd454bad69c
greicens <gsilva@mail.ccsf.edu>
2017-02-28 14:53:08 -0800

    fixes merge conflicts

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,5 @@
 class ReportsController < ApplicationController
   def index
-    @search = EntriesSearch.new(params[:search])
-    @entries = @search.scope
-    respond_to do |format|
-      format.html
-      format.pdf do
-        render pdf: "report_pdf.html.erb",   # Excluding ".pdf" extension.
-        template: "reports/report"
-      end
-    end
     category = Category.find_by_name(params[:category])
     @entries = current_user.entries.where(nil) #creates an anonymous scope
     @entries = @entries.category(category.id) if params[:category].present?
@@ -16,6 +7,13 @@ class ReportsController < ApplicationController
     @entries = @entries.entry_type(params[:entry_type]) if params[:entry_type].present?
     @entries = @entries.payment_type(params[:payment_type]) if params[:payment_type].present?
     @entries = @entries.between(params[:date_from], params[:date_to]) if params[:date_from].present? && params[:date_to].present?
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "report_pdf.html.erb",   # Excluding ".pdf" extension.
+        template: "reports/report"
+      end
+    end
   end
 
 end
