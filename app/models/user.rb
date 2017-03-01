a80de739480ceb3d3f053482295ba7c5dcf4c3d9
greicens <gsilva@mail.ccsf.edu>
2017-02-28 16:38:35 -0800

    adds facebook url

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :entries
   validates_uniqueness_of :username, :email
+  user.url = auth_hash['info']['urls'][user.provider.capitalize]
   include UsersHelper
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
