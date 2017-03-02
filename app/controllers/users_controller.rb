fb1cd430c6aa2515ac716f17b22af8618aa3ed4c
greicens <gsilva@mail.ccsf.edu>
2017-03-02 09:48:47 -0800

    adds password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :username,  :password, :name)
+    params.require(:user).permit(:email, :username,  :password, :name, :password_confirmation)
   end
 
 end
