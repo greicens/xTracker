71a6153142d39d5110ecdae2e7013fb68a9df081
greicens <gsilva@mail.ccsf.edu>
2017-03-01 18:11:39 -0800

    cleans up unnecessary code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -41,33 +41,7 @@ class SessionsController < ApplicationController
 
      redirect_to dashboard_path
    end
-    # # render text: request.env['omniauth.auth'].to_yaml
-    # begin
-    #   auth_hash = request.env['omniauth.auth']
-    #   puts "#{auth_hash} auth_hash from SessionsController"
-    #   @facebook_user = FacebookUser.find_or_create_by(
-    #                                                   uid: auth_hash['uid'],
-    #                                                   provider: auth_hash['provider'])
-    #   @user = @facebook_user
-    #
-    #   puts "#{@user} THIS IS THE USER from SessionsController"
-    #   login(@user)
-    #   # session[:user_id] = @user.id
-    #   flash[:success] = "Welcome, #{@user.name}!"
-    # rescue
-    #   flash[:warning] = "There was an error while trying to authenticate you..."
-    # end
-    # redirect_to '/'
   end
-    # begin
-    #   @user = User.from_omniauth(request.env['omniauth.auth'])
-    #   session[:user_id] = @user.id
-    #   flash[:success] = "Welcome, #{@user.name}!"
-    # rescue
-    #   flash[:warning] = "There was an error while trying to authenticate you..."
-    # end
-    # redirect_to root_path
-# end
 
   def destroy
     session[:user_id] = nil
